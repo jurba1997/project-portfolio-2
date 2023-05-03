e1ec1ceb750b820cb07306feab76308f92a29a86
jurba1997 <jurba2011@gmail.com>
2023-05-03 17:48:38 +0000

    add even listener to edit button

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+    const editBtn = note.querySelector(".edit");
+    const deleteBtn = note.querySelector(".delete");
+
+    const main = note.querySelector(".main");
+    const textArea = note.querySelector("textarea");
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
+    editBtn.addEventListener("click", () => {
+        main.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+    });
